dba014c5e3a8035cb251107ace43dec38e2666be
Christoher Resnick <chris.resnick@gmail.com>
2021-04-03 23:00:56 -0400

    resize

--- src/menu.js
+++ src/menu.js
@@ -4,13 +4,13 @@ import content from "./content";
 export default ({selected, setSelected}) => {
     return (
         <div className="menu">
-            {content.pages.map((c, i) => {
+            {content.pages.map((page, idx) => {
                 return (
                 <h2 
-                    className={selected===i?"linkSelected":"linkNotSelected"}
-                    onClick={() => setSelected(i)}
+                    className={selected===idx?"linkSelected":"linkNotSelected"}
+                    onClick={() => setSelected(idx)}
                 >
-                    {c.title}
+                    {page.title}
                 </h2>)
             })}
         </div>
