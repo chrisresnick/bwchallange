d4a998a784b7a448679b4f338beeac24e1ebdd48
Christoher Resnick <chris.resnick@gmail.com>
2021-04-03 22:14:43 -0400

    style

--- src/App.js
+++ src/App.js
@@ -10,16 +10,33 @@ function App() {
       className="main"
       style={{
         display:"flex",
+        flexDirection:"column",
+        justifyContent:"space-between",
         backgroundImage: `url("./imgs/${pages[selected].blocks[0].background}")`,
         backgroundSize:"cover",
         height: "100vh",
-        width: "100vw"
+        width: "100vw",
+        paddingTop: "5vh",
+        paddingLeft: "5vw",
+        paddingRight: "5vw"
       }}
     >
-      <div className="marquee">
+      <div className="header">
         <Menu selected={selected} setSelected={setSelected}/>
+        <div className="contact">Contact Us</div>
       </div>
-      <div>
+      <div className="marquee">
+        <div className="headline">
+            {pages[selected].blocks[0].headline}
+        </div>
+        <div className="subhead">
+          {pages[selected].blocks[0].subhead}
+        </div>
+      </div>
+      <div className="callToAction">
+        <div className="ctaInner">
+          {pages[selected].blocks[0].cta}  
+        </div>
       </div>
     </div>
   );
