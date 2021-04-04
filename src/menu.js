1e82007dcb195a1319ecb2b2f0c59b663c6ed71e
Christoher Resnick <chris.resnick@gmail.com>
2021-04-04 12:23:53 -0400

    indednt

--- src/menu.js
+++ src/menu.js
@@ -5,13 +5,14 @@ const Menu = ({selected, setSelected, pages}) => {
         <div className="menu">
             {pages.map((page, idx) => {
                 return (
-                <h2 
-                    key={`link${idx}`}
-                    className={selected===idx?"linkSelected":"linkNotSelected"}
-                    onClick={() => setSelected(idx)}
-                >
-                    {page.title}
-                </h2>)
+                    <h2 
+                        key={`link${idx}`}
+                        className={selected===idx?"linkSelected":"linkNotSelected"}
+                        onClick={() => setSelected(idx)}
+                    >
+                        {page.title}
+                    </h2>
+                )
             })}
         </div>
     )
